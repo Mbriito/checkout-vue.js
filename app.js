b8d31a6df1e163304ebdf06461ca5bcaaae02b10
Mbriito <mariabrito070@gmail.com>
2022-07-16 14:16:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = {
+    data() {
+        return {
+            name: 'Maria',
+            lastName: 'Brito - Especialista',
+            product: {
+                title: 'Adesivo1',
+                description: 'Adesivos coloridos',
+                Image: './assets/img/adesivo1.jpg',
+            },
+            product02: {
+                title: 'Adesivo2',
+                description: 'Adesivos coloridos',
+                Image: './assets/img/adesivo2.jpg',
+            }
+        }
+    }
